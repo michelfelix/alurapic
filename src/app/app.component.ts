5885377d72dcf20506ce01407346115f0b2baf58
Michel Felix <micheldias9@gmail.com>
2021-07-19 16:11:39 -0300

    Atribuindo ao projeto imagem e descrição através de expressão pelo app.component.ts.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'alurapic';
+  description='Leão';
+  url='https://img.r7.com/images/moya-leao-branco-albino-atracao-glen-garriff-conservation-lion-sanctuary-23032021161517683';
 }
